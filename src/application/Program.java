10e990522da8edae212be04949522c521d026783
Cicero Jonyedson <cicerojonyedison@gmail.com>
2019-11-09 13:50:06 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,7 +16,6 @@ public class Program {
 		
 		System.out.println("=== TEST 1: Seller findById ====");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 		
 		System.out.println("\n=== TEST 2: Seller findByDepartment ====");
@@ -25,15 +24,25 @@ public class Program {
 		for (Seller obj : sellers) {
 			System.out.println(obj);
 		}
+		
 		System.out.println("\n=== TEST 3: Seller findAll====");
 		sellers = sellerDao.findAll();
 		for (Seller obj : sellers) {
 			System.out.println(obj);
 		}
+		
 		System.out.println("\n=== TEST 4: Seller insert ====");
 		Seller newSeller = new Seller(null,"Greg","greg@gmail.com", new Date() ,4000.0,department );
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted ! New id = " + newSeller.getId());
+		
+		
+		System.out.println("\n=== TEST 5: Seller Update ====");
+		seller = sellerDao.findById(8);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
 	}
 
 }
